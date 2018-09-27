8446b30b7632610d51c60857f8d5adcd2cfa8144
zhangrongwu <zhangrongwuios@icloud.com>
2018-09-27 16:32:15 +0800

    修改

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home-view has-header">
+    <button @click="nativeMethod">按钮点击</button>
     <sub-nav mold="quickNav"></sub-nav>
     <list mold="thumbnail" :items="events"></list>
     <infinite-loading :on-infinite="onInfinite" ref="infiniteLoading">
@@ -9,44 +10,52 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions } from "vuex";
 
-import InfiniteLoading from 'vue-infinite-loading'
-import SubNav from '../components/SubNav'
-import List from '../components/List'
-import Loading from '../components/Loading'
+import InfiniteLoading from "vue-infinite-loading";
+import SubNav from "../components/SubNav";
+import List from "../components/List";
+import Loading from "../components/Loading";
 
 export default {
-  name: 'home-view',
-  components: { SubNav, List, InfiniteLoading, Loading },
-  data () {
-    return {}
-  },
-  computed: {
-    // Getting Vuex State from store/modules/activities
-    ...mapState({
-      events: state => state.activities.events
-    })
-  },
-  methods: {
-    // Using vue-infinite-loading
-    onInfinite () {
-      setTimeout(() => {
-        this.loadMore()
-        this.$refs.infiniteLoading.$emit('$InfiniteLoading:loaded')
-      }, 1000)
+    name: "home-view",
+    components: { SubNav, List, InfiniteLoading, Loading },
+    data() {
+        return {};
     },
-    // Dispatching Actions
-    ...mapActions([
-      'loadMore'
-    ])
-  }
-}
+    computed: {
+        // Getting Vuex State from store/modules/activities
+        ...mapState({
+            events: state => state.activities.events
+        })
+    },
+    methods: {
+        // Using vue-infinite-loading
+        onInfinite() {
+            setTimeout(() => {
+                this.loadMore();
+                this.$refs.infiniteLoading.$emit("$InfiniteLoading:loaded");
+            }, 1000);
+        },
+        nativeMethod() {
+            try {
+                window.webkit.messageHandlers.wkWebViewCountent.postMessage(
+                    null
+                );
+                alert("调用");
+            } catch (error) {
+                alert("打开原生方法失败");
+            }
+        },
+        // Dispatching Actions
+        ...mapActions(["loadMore"])
+    }
+};
 </script>
 
 <style lang="scss" scoped>
 .sub-nav {
-  margin: 0 1.8rem;
-  padding-top: 0.2rem;
+    margin: 0 1.8rem;
+    padding-top: 0.2rem;
 }
 </style>
